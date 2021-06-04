3dd88b94205c687f0dc91cfa961778397e437dcf
olivier-sonrel <o.sonrel@it-students.fr>
2021-06-04 12:27:36 +0200

    tout

---
+++ ex08/jennyssecretmessage_v2.py
@@ -0,0 +1,9 @@
+prenom = input("Quel est votre prénom ?\n")
+if prenom == "Johnny":
+  print ("Je te déteste !")
+if prenom == "Paul":
+  print ("Hello my love !")
+if prenom == "Marc":    
+  print ("Hello my love !")
+if prenom == "Ismael": 
+  print ("Salut, désolé pas le temps de te parler.")
